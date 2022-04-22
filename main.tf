e37cec6622ee26ef5b50dc2dc0db0bc26e5f05c4
Sony K Sunny <52253656+sony7760@users.noreply.github.com>
2022-04-22 16:52:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,13 @@ terraform {
   required_providers {
     virtualbox = {
       source = "terra-farm/virtualbox"
-      version = "0.2.1"
+      version = "0.2.2-alpha.1"
     }
   }
+}
+
+provider "virtualbox" {
+  # Configuration options
 }
 
 resource "virtualbox_vm" "main" {
